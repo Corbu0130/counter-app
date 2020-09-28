1d785d531bc377199f876482e584afc5729d2e5e
corbu0130 <johncorbeta1997@gmail.com>
2020-09-29 05:15:33 +0800

    Changed navbar href.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,21 +3,22 @@ import Counter from "./counter"
 
 class Counters extends Component {
     render() { 
-    return ( 
-        <div>
-            <button 
-                onClick={this.props.onReset}
-                className="btn btn-primary btn-sm m-2">Reset</button>
-            {this.props.counters.map(counter => {
-                return <Counter
-                    key={counter.id}
-                    counter={counter}
-                    onDelete={this.props.onDelete}
-                    onIncrement={this.props.onIncrement} />
-            })}
-        </div>
-        );
-    }
+        const {counters, onIncrement, onDelete, onReset} = this.props
+        return ( 
+            <div>
+                <button 
+                    onClick={onReset}
+                    className="btn btn-primary btn-sm m-2">Reset</button>
+                {counters.map(counter => {
+                    return <Counter
+                        key={counter.id}
+                        counter={counter}
+                        onDelete={onDelete}
+                        onIncrement={onIncrement} />
+                })}
+            </div>
+            );
+        }
 }
  
 export default Counters;
